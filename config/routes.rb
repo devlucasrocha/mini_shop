3b78217e4e6bf87bc4cf8e4a53cff77924a3ccda
devlucasrocha <lucasgeorgerocha@gmail.com>
2019-03-02 17:39:24 -0300

    Fixing and improving somethings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'contact/send_message'
+  get 'contact/send_message/:id', to: 'contact#send_message', as: 'contact_to_buy'
   resources :products
   get 'shop/home'
   root to: "shop#home"
